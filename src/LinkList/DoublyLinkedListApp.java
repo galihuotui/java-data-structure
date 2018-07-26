8f7819abccfe87f5a4be087a6bd7c7018705bd63
cuikangyuan <cuikangyuan@51tiangou.com>
2018-07-26 17:52:34 +0800

    add doublely linked list

---
+++ src/LinkList/DoublyLinkedListApp.java
@@ -0,0 +1,201 @@
+package LinkList;
+
+public class DoublyLinkedListApp {
+
+    public static void main(String[] args) {
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+        doublyLinkedList.insertFirst(22);
+        doublyLinkedList.insertFirst(44);
+        doublyLinkedList.insertFirst(66);
+
+        doublyLinkedList.insertLast(11);
+        doublyLinkedList.insertLast(33);
+        doublyLinkedList.insertLast(55);
+
+        doublyLinkedList.displayForward();
+        doublyLinkedList.displayBackward();
+
+        doublyLinkedList.deleteFirst();
+        doublyLinkedList.deleteLast();
+        doublyLinkedList.deleteKey(11);
+
+        doublyLinkedList.displayForward();
+
+        doublyLinkedList.insertAfter(22, 77);
+        doublyLinkedList.insertAfter(22, 88);
+
+        doublyLinkedList.displayForward();
+
+
+    }
+
+    static class Link {
+        public long dData;
+        public Link next;
+        public Link previous;
+
+        public Link(long d) {
+            dData = d;
+        }
+
+        public void displayLink() {
+            System.out.print(dData + " ");
+        }
+    }
+
+    static class DoublyLinkedList {
+        private Link first;
+        private Link last;
+
+        public DoublyLinkedList() {
+            first = null;
+            last = null;
+        }
+
+        public boolean isEmpty() {
+            return first == null;
+        }
+
+
+        public void insertFirst(long dd) {
+
+            Link newLink = new Link(dd);
+
+            if (isEmpty()) {
+                last = newLink;
+            } else {
+                first.previous = newLink;
+            }
+
+            newLink.next = first;
+            first = newLink;
+        }
+
+        public void insertLast(long dd) {
+            Link newLink = new Link(dd);
+
+            if (isEmpty()) {
+                first = newLink;
+            } else {
+                last.next = newLink;
+                newLink.previous = last;
+            }
+
+            last = newLink;
+        }
+
+        public Link deleteFirst() {
+            Link temp = first;
+
+            if (first.next == null) {
+                last = null;
+            } else {
+                first.next.previous = null;
+            }
+
+            first = first.next;
+
+            return temp;
+        }
+
+        public Link deleteLast() {
+            Link temp = last;
+
+            if (first.next == null) {
+                first = null;
+            } else {
+                last.previous.next = null;
+            }
+
+            last = last.previous;
+            return temp;
+        }
+
+        public boolean insertAfter(long key, long dd) {
+
+            Link current = first;
+
+            while (current.dData != key) {
+
+                current = current.next;
+
+                if (current == null) {
+                    return false;
+                }
+            }
+
+            Link newLink = new Link(dd);
+
+            if (current == last) { //if last link
+
+                newLink.next = null;
+                last = newLink;
+
+            } else {
+
+                newLink.next = current.next;
+                current.next.previous = newLink;
+            }
+
+            newLink.previous = current;
+            current.next = newLink;
+
+            return true;
+        }
+
+        public Link deleteKey(long key) {
+
+            Link current = first;
+            while (current.dData != key) {
+
+                current = current.next;
+
+                if (current == null) {
+                    return null;
+                }
+            }
+
+            if (current == first) {
+                first = current.next;
+            } else {
+                current.previous.next = current.next;
+            }
+
+            if(current == last) {
+                last = current.previous;
+            } else {
+                current.next.previous = current.previous;
+            }
+
+            return current;
+        }
+
+        public void displayForward() {
+            System.out.print("List (first --> last): ");
+
+            Link current = first;
+
+            while (current != null) {
+                current.displayLink();
+                current = current.next;
+            }
+
+            System.out.println(" ");
+        }
+
+        public void displayBackward() {
+            System.out.print("List (last --> first): ");
+
+            Link current = last;
+
+            while (current != null) {
+                current.displayLink();
+                current = current.previous;
+            }
+
+            System.out.println(" ");
+        }
+
+    }
+}
