775b597d2f94bdbdd073768fe067a0c9e4526f15
cuikangyuan <cuikangyuan@51tiangou.com>
2019-07-05 10:05:42 +0800

    modify quick sort

--- src/quick_sort/Case1.java
+++ src/quick_sort/Case1.java
@@ -4,22 +4,24 @@ public class Case1 {
 
     public static void main(String[] args) {
 
-        //int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-        int[] array = {9, 1, 7, 3, 8, 4, 7, 2, 1};
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        //int[] array = {9, 1, 7, 3, 8, 4, 7, 2, 1};
 
         Case1 case1 = new Case1();
 
-        case1.printArray(array);
+        //case1.printArray(array);
 
         case1.quickSort(array, 0, array.length - 1);
 
-        case1.printArray(array);
+        //case1.printArray(array);
 
 
     }
 
 
     public void quickSort(int[] array, int low, int high) {
+
+        System.out.println("quickSort start low index ->  " + low + " high index ->   " + high);
 
         if (low < high) {
 
@@ -36,7 +38,13 @@ public class Case1 {
 
             int index = partition(array, low, high);
 
+            System.out.println("partition then get index " + index);
+
+            System.out.println("call quickSort low index ->  " + low + " high index ->  " + (index - 1));
+
             quickSort(array, low, index - 1);
+
+            System.out.println("call quickSort low index ->  " + (index + 1) + " high index ->  " + high);
 
             quickSort(array, index + 1, high);
 
@@ -49,6 +57,8 @@ public class Case1 {
     public int partition(int[] array, int low, int high) {
 
         System.out.println("array before partition : ");
+
+        printArray(array);
 
         System.out.println("partition: low index: " + low + " high index: " + high + " ");
 
@@ -65,6 +75,9 @@ public class Case1 {
 
         while (left < right) {
 
+            System.out.println("out loop running : " + " left index -> " + left + " right index -> " + right);
+
+
             while (array[left] < pivot) {
 
                 if (left == high) {
@@ -76,6 +89,9 @@ public class Case1 {
 
             }
 
+            System.out.println("left loop finish : " + " left index -> " + left + " right index -> " + right);
+
+
             while (array[right] > pivot) {
 
                 if (right == low) {
@@ -86,13 +102,25 @@ public class Case1 {
 
             }
 
+            System.out.println("right loop finish : " + " left index -> " + left + " right index -> " + right);
+
+
             if (left < right) {
 
                 System.out.println("swap left and right : " + " left value -> " + array[left] + " right value -> " + array[right]);
 
                 swap(array, left, right);
+
+                System.out.println("current array : ");
+
+                printArray(array);
+
             }
         }
+
+
+        System.out.println("stop out loop : " + " left index -> " + left + " right index -> " + right);
+
 
         System.out.println("swap low and right : " + " low value -> " + array[low] + " right value -> " + array[right]);
 
