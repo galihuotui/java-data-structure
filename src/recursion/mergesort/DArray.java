9d32f040228d76503653725ca262b892ae6242a2
cuikangyuan <cuikangyuan@51tiangou.com>
2019-10-09 09:38:20 +0800

    add test code of merge sort

--- src/recursion/mergesort/DArray.java
+++ src/recursion/mergesort/DArray.java
@@ -42,11 +42,11 @@ public class DArray {
 
     public void recMergeSort(long[] workSpace, int lowerBound, int upperBound) {
 
-        System.out.println("Entering" + lowerBound + "-" +upperBound);
+        System.out.println("Entering " + lowerBound + "-" +upperBound);
 
         if (lowerBound == upperBound) { // if range is 1
 
-            System.out.println("Base-Case " + lowerBound + "-" +upperBound);
+            System.out.println("Base-Case Return " + lowerBound + "-" +upperBound);
 
             return;                     //no use sorting
 
@@ -60,18 +60,27 @@ public class DArray {
             recMergeSort(workSpace, lowerBound, mid);
 
             //sort high half
-            System.out.println("Will sort low half of " + lowerBound + "-" +upperBound);
+            System.out.println("Will sort high half of " + lowerBound + "-" +upperBound);
             recMergeSort(workSpace, mid+1, upperBound);
 
             //merge them
             System.out.println("Will merge halves into " + lowerBound + "-" +upperBound);
+
+
             merge(workSpace, lowerBound, mid + 1, upperBound);
+
+
+            System.out.print("Return " + lowerBound + "-" +upperBound + " ");
+            display();
 
         }
 
 
     }
 
+    //lowerPrt : the start index of the low half array
+    //highPtr : the start index of the high half array
+    //upperBound:
     public void merge(long[] workSpace, int lowerPrt, int highPtr, int upperBound) {
 
         int j = 0;
