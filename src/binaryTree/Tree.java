b6b1d1bc0dedfc8ba1e7beb911170d32d99f9b5e
cuikangyuan <cuikangyuan@outlook.com>
2020-03-12 20:54:12 +0800

    add binary tree code

--- src/binaryTree/Tree.java
+++ src/binaryTree/Tree.java
@@ -52,9 +52,129 @@ public class Tree {
         }
     }
 
-    public void delete(int id) {
-        //TODO
+
+
+    public void inOrder(Node localRoot) {
+        if (localRoot != null) {
+            inOrder(localRoot.leftChild);
+
+            System.out.println(localRoot.iData + " ");
+
+            inOrder(localRoot.rightChild);
+        }
     }
 
+    public Node minimum() {
+        Node current, last = null;
+        current = root;
+
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
+        }
+
+        return last;
+    }
+
+    public Node maximum() {
+        Node current, last = null;
+        current = root;
+
+        while (current != null) {
+            last = current;
+            current = current.rightChild;
+        }
+
+        return last;
+    }
+
+    private Node getSuccessor(Node delNode) {
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.rightChild;
+
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+
+        if (successor != delNode.rightChild) {
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+
+        return successor;
+    }
+
+    public boolean delete(int key) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+
+        while (current.iData != key) {
+            parent = current;
+
+            if (key < current.iData) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+
+            if (current == null) {
+                return false;
+            }
+        }
+
+        //find node to delete
+        //if no children simply delete it
+        if (current.leftChild == null && current.rightChild == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.leftChild = null;
+            } else {
+                parent.rightChild = null;
+            }
+        } else if (current.rightChild == null) {
+            //if no right child replace with left subtree
+            if (current == root) {
+                root = current.leftChild;
+            } else if (isLeftChild) {
+                parent.leftChild = current.leftChild;
+            } else {
+                parent.rightChild = current.leftChild;
+            }
+        } else if (current.leftChild == null) {
+            //if no left child replace with right subtree
+            if (current == root) {
+                root = current.rightChild;
+            } else if (isLeftChild) {
+                parent.leftChild = current.rightChild;
+            } else {
+                parent.rightChild = current.rightChild;
+            }
+        } else {
+            //two children so replace with inorder successor
+            //get successor of node to delete
+            Node successor = getSuccessor(current);
+
+            //connect parent of current to successor instead
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.leftChild = successor;
+            } else {
+                parent.rightChild = successor;
+            }
+
+            successor.leftChild = current.leftChild;
+            //successor cannot have a left child
+        }
+
+        return true;
+    }
 
 }
