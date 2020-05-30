b3b06be66f0bc1998ac34fb65763942ff48bba3d
cuikangyuan <cuikangyuan@outlook.com>
2020-05-30 23:01:12 +0800

    add tree test code

---
+++ src/tree234/DataItem.java
@@ -0,0 +1,16 @@
+package tree234;
+
+public class DataItem {
+
+    public long dData;
+
+    public DataItem(long dData) {
+        this.dData = dData;
+    }
+
+    public void displayItem() {
+        System.out.print(" /" + dData);
+    }
+
+
+}
