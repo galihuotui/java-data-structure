6482df2e2fe9db5ec83826eb9f741009c1f54747
cuikangyuan <cuikangyuan@jd.com>
2021-10-24 17:35:40 +0800

    add test code

--- src/nextGreaterElement/Client.java
+++ src/nextGreaterElement/Client.java
@@ -6,7 +6,11 @@ public class Client {
 
     public static void main(String[] args) {
 
-        new Client().nextGreaterElement();
+        //new Client().nextGreaterElement();
+
+        //new Client().dailyTemperatures();
+
+        new Client().nextGreaterElementInLoopArray();
     }
 
     public void nextGreaterElement() {
@@ -27,6 +31,46 @@ public class Client {
         for (int i : s) {
             System.out.print(i + " ");
         }
-
     }
+
+    public void dailyTemperatures() {
+
+        int[] t = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] result = new int[t.length];
+
+        Stack<Integer> stack = new Stack();
+        for (int i = t.length - 1; i >= 0; i--) {
+            while (!stack.empty() && t[stack.peek()] <= t[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? 0 : stack.peek() - i; // get the distance
+            stack.push(i);// add index, not the element
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+
+    public void nextGreaterElementInLoopArray() {
+
+        int[] a = {2, 1, 2, 4, 3};
+        int n = a.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack();
+
+        for (int i = n * 2 - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= a[i % n]) {
+                stack.pop();
+            }
+            result[i % n] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(a[i % n]);
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
 }
