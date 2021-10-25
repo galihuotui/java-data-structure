69e229a2c56c84d0e9be2645231dd29cb9d088b4
cuikangyuan <cuikangyuan@jd.com>
2021-10-25 21:25:40 +0800

    add test code

--- src/autoBox/Client.java
+++ src/autoBox/Client.java
@@ -15,7 +15,7 @@ public class Client {
         System.out.println(e == f);//false
         System.out.println(c == (a + b));//true
         System.out.println(c.equals(a + b));//true
-        System.out.println(g == (a + b));//true
+        System.out.println(g == (a + b));//true iadd -> i2l -> lcmp
         System.out.println(g.equals(a + b));//false
     }
 }
